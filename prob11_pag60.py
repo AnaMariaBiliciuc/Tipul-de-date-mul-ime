fd73c317fd161c6352fe3a345f77bcc001b802fc
Ana Maria Biliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2021-11-17 17:29:11 +0200

    Add files via upload

---
+++ prob11_pag60.py
@@ -0,0 +1,10 @@
+A={'A' 'B' 'C' 'D'}
+from itertools import combinations
+A1=set(combinations(A,1))
+print('Submultimile formate dintr-un singur element: ',A1)
+A2=set(combinations(A,2))
+print('Submultimile formate din 2 elemente: ', A2)
+A3=set(combinations(A,3))
+print('Submultimile formate din 3 elemente: ', A3)
+A4=set(combinations(A,4))
+print('Submultimile formate din 4 elemente: ', A4)
